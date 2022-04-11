a7cd78a66adb860253092a5c25f7573271d04e91
Dennis Ogiermann <termi-official@users.noreply.github.com>
2022-04-11 16:08:27 +0200

    Trying to improve interface for HHT model.

--- src/Thunderbolt.jl
+++ src/Thunderbolt.jl
@@ -1,5 +1,7 @@
 module Thunderbolt
 
+using StaticArrays
+
 abstract type AbstractIonChannel end;
 
 abstract type MarkovTypeIonChannel <: AbstractIonChannel end;
@@ -10,16 +12,49 @@ Helper to properly dispatch individual gating variables.
 abstract type HodgkinHuxleyTypeGate end;
 
 """
-The classical gate formulation has the form
+Parameters for a generic sigmoid function of the form
 
-τₖ(φₘ)∂ₜsₖ = σ(φₘ) - φₘ
+σ(x) = \frac{A + B x}{C + D \exp{\frac{E + F x}{G}}}
 """
-struct ClassicalHodgkinHuxleyGate <: HodgkinHuxleyTypeGate
-
+struct GenericSigmoidParameters where {T}
+    A::T
+    B::T
+    C::T
+    D::T
+    E::T
+    F::T
+    G::T
 end
 
+@inline σ(x::T1, p::GenericSigmoidParameters{T2}) where {T1, T2} = (p.A + p.B*x)/(p.C + p.D*exp(p.E + p.F*x) / p.G)
+
 """
-Ion channels with diagonal, semi-affine internal structure.
+The classical gate formulation is stated in the normalized affine form:
+
+∂ₜ𝐬ₖ = αₖ(φₘ)𝐬ₖ + βₖ(φₘ)
+
+where αₖ(φₘ) = \frac{A + B φₘ}{C + D \exp{\frac{E + F φₘ}{G}}}
+
+Note that the original formulation is
+
+∂ₜ𝐬ₖ = aₖ(φₘ)𝐬ₖ + bₖ(φₘ)(1 - 𝐬ₖ)
+
+where αₖ = aₖ - bₖ and βₖ = bₖ.
+"""
+struct GenericHodgkinHuxleyGate <: HodgkinHuxleyTypeGate where {T}
+    αₚ::GenericSigmoidParameters{T}
+    βₚ::GenericSigmoidParameters{T}
+end
+
+@inline α(g::GenericHodgkinHuxleyGate{T}, φₘ::T) where {T} = σ(φₘ, g.αₚ)
+@inline β(g::GenericHodgkinHuxleyGate{T}, φₘ::T) where {T} = σ(φₘ, g.βₚ)
+
+# Spatially varying parameters
+@inline α(g::GenericHodgkinHuxleyGate{T1}, φₘ::T2, x::T3) where {T1,T2,T3} = σ(φₘ, g.αₚ(x))
+@inline β(g::GenericHodgkinHuxleyGate{T1}, φₘ::T2, x::T3) where {T1,T2,T3} = σ(φₘ, g.βₚ(x))
+
+"""
+Probabilistic ion channels with diagonal, semi-affine internal structure.
 
 ∂ₜ𝐬₁ = g₁(φₘ, 𝐬) = α₁(φₘ)𝐬₁ + β₁(φₘ)
      .
@@ -31,16 +66,29 @@ They can be derived as special cases of Markov type ion channels with
 tensor-product structure (TODO citation). 𝐬 is called the gating vector
 and its entries are the gating variables.
 """
-struct HodgkinHuxleyTypeIonChannel <: AbstractIonChannel
-    gates::Vector{HodgkinHuxleyTypeGate}
+struct HodgkinHuxleyTypeIonChannel <: AbstractIonChannel where {NGates}
+    gates::SVector{NGates, HodgkinHuxleyTypeGate}
+    powers::SVector{NGates, Int}
 end;
 
-@inline function g(::HodgkinHuxleyTypeGate, φₘ::T, 𝐬ᵢ::T) where {T}
-    α(::HodgkinHuxleyTypeGate, φₘ)*𝐬 + β(::HodgkinHuxleyTypeGate, φₘ)
+@inline function g(gate::HodgkinHuxleyTypeGate, φₘ::T, 𝐬ᵢ::T) where {T}
+    α(gate, φₘ)*𝐬ᵢ + β(gate, φₘ)
 end
 
-@inline function g(::HodgkinHuxleyTypeGate, φₘ::T, 𝐬ᵢ::T, x::AbstractVector{T}) where {T}
-    α(::HodgkinHuxleyTypeGate, φₘ, x)*𝐬 + β(::HodgkinHuxleyTypeGate, φₘ, x)
+@inline function g(gate::HodgkinHuxleyTypeGate, φₘ::T, 𝐬ᵢ::T, x::AbstractVector{T}) where {T}
+    α(gate, φₘ, x)*𝐬ᵢ + β(gate, φₘ, x)
+end
+
+"""
+Ohmic current of the form
+
+Iⱼ = ̄gⱼ pⱼ (φₘ - Eⱼ)
+
+where ̄gⱼ is the maximal conductance, pᵢ the open probability of the associated channel and Eⱼ the equilibrium potential.
+"""
+struct OhmicCurrent where {T, NChannels}
+    g::T
+    channels::SVector{NChannels, HodgkinHuxleyTypeIonChannel}
 end
 
 abstract type AbstractIonicModel end;
@@ -110,11 +158,11 @@ Transformed bidomain model with the structure
 This formulation is a transformation of the parabolic-parabolic
 form (c.f. TODO citation) and has been derived by (TODO citation) first.
 """
-struct ParabolicEllipticBidomainModel <: AbstractEPModel
-    χ
-    Cₘ
-    κᵢ
-    κₑ
+struct ParabolicEllipticBidomainModel <: AbstractEPModel where {T1,T2}
+    χ::T1
+    Cₘ::T1
+    κᵢ::T2
+    κₑ::T2
     stim::AbstractStimulationProtocoll
     ion::AbstractIonicModel
 end
@@ -130,10 +178,10 @@ Simplification of the bidomain model with the structure
 assumption is violated we can construct optimal κ (TODO citation+example) for the
 reconstruction of φₘ.
 """
-struct MonodomainModel <: AbstractEPModel
-    χ
-    Cₘ
-    κ
+struct MonodomainModel <: AbstractEPModel where {T1,T2}
+    χ::T1
+    Cₘ::T1
+    κ::T2
     stim::TransmembraneStimulationProtocol
     ion::AbstractIonicModel
 end
