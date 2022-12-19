f36379f192ff42545431741296a73125ef2c8740
termi-official <termi-official@users.noreply.github.com>
2022-12-19 15:59:18 +0100

    Add fit for HO2009 and LinYin1998 (passive) to LinYin1998 figure 4.

--- test/fimh_2023_linyin_new_model.jl
+++ test/fimh_2023_linyin_new_model.jl
@@ -1,11 +1,12 @@
 include("contractile_cuboid.jl")
 
+using LsqFit
 using GLMakie
 using DelimitedFiles
 
 # Helper to plot equibiaxial model
 function plot_equibiaxial(axis, P; λₘᵢₙ = 1.08, λₘₐₓ = 1.3, num_samples=25)
-    λ₀ = λₘᵢₙ-1.0 # correction in LinYin?
+    λ₀ = 0.0 #λₘᵢₙ-1.0 # correction in LinYin?
     λset = range(λₘᵢₙ, λₘₐₓ, length=num_samples)
     lines!(axis, λset, λ -> P(Tensor{2,3,Float64}((
                                 (λ-λ₀),0.0,0.0,
@@ -36,9 +37,9 @@ linyin_original_active = GeneralizedHillModel(
     PelceSunLangeveld1995Model()
 )
 
-f₀ = Vec((1.0, 0.0, 0.0))
-s₀ = Vec((0.0, 1.0, 0.0))
-n₀ = Vec((0.0, 0.0, 1.0))
+f₀ = Tensors.Vec((1.0, 0.0, 0.0))
+s₀ = Tensors.Vec((0.0, 1.0, 0.0))
+n₀ = Tensors.Vec((0.0, 0.0, 1.0))
 
 
 f = Figure()
@@ -46,53 +47,114 @@ ax = Axis(f[1,1])
 
 plot_equibiaxial(ax, F -> constitutive_driver(F, f₀, s₀, n₀, 0.0, linyin_original_passive)[1])
 linyin1998_4_fiber_passive = readdlm("./data/experiments/LinYin1998/LinYin_1998_Fig4_Fiber_Passive.csv", ',')
-linyin1998_4_fiber_passive[:,2] .*= 10.0 #(scale)
 scatter!(ax, linyin1998_4_fiber_passive)
 linyin1998_4_sheet_passive = readdlm("./data/experiments/LinYin1998/LinYin_1998_Fig4_Sheet_Passive.csv", ',')
-linyin1998_4_sheet_passive[:,2] .*= 10.0 #(scale)
 scatter!(ax, linyin1998_4_sheet_passive)
 
 plot_equibiaxial(ax, F -> constitutive_driver(F, f₀, s₀, n₀, 0.0, linyin_original_active)[1])
 linyin1998_4_fiber_active = readdlm("./data/experiments/LinYin1998/LinYin_1998_Fig4_Fiber_Active.csv", ',')
-linyin1998_4_fiber_active[:,2] .*= 10.0 #(scale)
 scatter!(ax, linyin1998_4_fiber_active)
 linyin1998_4_sheet_active = readdlm("./data/experiments/LinYin1998/LinYin_1998_Fig4_Sheet_Active.csv", ',')
-linyin1998_4_sheet_active[:,2] .*= 10.0 #(scale)
 scatter!(ax, linyin1998_4_sheet_active)
-
-f
 
 #TODO (Fig 12, from Lin&Yin 1998)
 # 1. refit parameters of the proposed energies (passive with Levenberg-Marquard and active with Luigi's approach)
-function equibiaxial_evaluation_passive(λ, p, model_construction::Function)
+function equibiaxial_evaluation_passive(λset, p, model_construction::Function)
     mp = model_construction(p)
-    # Assume incompressibility to construct deformation gradient
-    F1 = Tensor{2,3,Float64}((
-        λ[1],0.0,0.0,
-        0.0,λ[1],0.0,
-        0.0,0.0,1.0/λ[1]^2))
-    P1 = constitutive_driver(F1, f₀, s₀, n₀, 0.0, mp)[1]
+    Pset = zero(λset)
+    for i ∈ 1:size(λset,1)
+        λ = λset[i,:]
+        # Assume incompressibility to construct deformation gradient
+        F1 = Tensor{2,3,Float64}((
+            λ[1],0.0,0.0,
+            0.0,λ[1],0.0,
+            0.0,0.0,1.0/λ[1]^2))
+        P1 = constitutive_driver(F1, f₀, s₀, n₀, 0.0, mp)[1]
+        Pset[i,1] = P1[1,1]
 
-    # Assume incompressibility to construct deformation gradient
-    F2 = Tensor{2,3,Float64}((
-        λ[2],0.0,0.0,
-        0.0,λ[2],0.0,
-        0.0,0.0,1.0/λ[2]^2))
-    P2 = constitutive_driver(F2, f₀, s₀, n₀, 0.0, mp)[1]
-
-    P1[1,1], P2[2,2]
+        # Assume incompressibility to construct deformation gradient
+        F2 = Tensor{2,3,Float64}((
+            λ[2],0.0,0.0,
+            0.0,λ[2],0.0,
+            0.0,0.0,1.0/λ[2]^2))
+        P2 = constitutive_driver(F2, f₀, s₀, n₀, 0.0, mp)[1]
+        Pset[i,2] = P2[2,2]
+    end
+    # not sure why we have to flatten the matrix here...
+    return vec(Pset)
 end
 
-# TODO preprocessing of experimental data - shift stretchs to equilibrium via passive measurement data
-fit = curve_fit((λ,p_)->equibiaxial_evaluation_passive(λ, p_, p->GeneralizedHillModel(
+# preprocessing of experimental data 
+# 1. shift stretchs to equilibrium via passive measurement data
+λshift = minimum(linyin1998_4_fiber_passive[:,1])-1.0
+linyin1998_4_fiber_passive[:,1] .-= λshift
+linyin1998_4_sheet_passive[:,1] .-= λshift
+linyin1998_4_fiber_active[:,1] .-= λshift
+linyin1998_4_sheet_active[:,1] .-= λshift
+
+# 2. weird unit to kPa
+# linyin1998_4_fiber_passive[:,2] .*= 10.0 #(scale)
+# linyin1998_4_sheet_passive[:,2] .*= 10.0 #(scale)
+# linyin1998_4_fiber_active[:,2] .*= 10.0 #(scale)
+# linyin1998_4_sheet_active[:,2] .*= 10.0 #(scale)
+
+model = (λ,p_)->equibiaxial_evaluation_passive(λ, p_, p->GeneralizedHillModel(
     LinYinPassiveModel(p[1],p[2],p[3],p[4],NullCompressionModel()),
     ActiveMaterialAdapter(NullEnergyModel()),
     RLRSQActiveDeformationGradientModel(0.5),
-    PelceSunLangeveld1995Model()),
-    linyin1998_4_fiber_passive[:,1],
-    linyin1998_4_fiber_passive[:,2],
+    PelceSunLangeveld1995Model())
+)
+xdata = hcat(linyin1998_4_fiber_passive[:,1], linyin1998_4_sheet_passive[:,1])
+# not sure why we have to flatten the matrix here...
+ydata = vec(hcat(linyin1998_4_fiber_passive[:,2], linyin1998_4_sheet_passive[:,2]))
+fit = curve_fit(model,
+    xdata,
+    ydata,
+    # Take original parameters as initial guess
+    [linyin_original_passive.passive_spring.C₁, linyin_original_passive.passive_spring.C₂, linyin_original_passive.passive_spring.C₃, linyin_original_passive.passive_spring.C₄]
+)
 
-))
+p = fit.param
+linyin_refit_passive = GeneralizedHillModel(
+    LinYinPassiveModel(p[1], p[2], p[3], p[4], NullCompressionModel()),
+    ActiveMaterialAdapter(NullEnergyModel()),
+    RLRSQActiveDeformationGradientModel(0.5),
+    PelceSunLangeveld1995Model()
+)
+
+f2 = Figure()
+ax2 = Axis(f2[1,1])
+
+plot_equibiaxial(ax2, F -> constitutive_driver(F, f₀, s₀, n₀, 0.0, linyin_refit_passive)[1]; λₘᵢₙ=minimum(xdata), λₘₐₓ=maximum(xdata))
+
+# ------
+
+model = (λ,p_)->equibiaxial_evaluation_passive(λ, p_, p->GeneralizedHillModel(
+    HolzapfelOgden2009Model(p[1],p[2],p[3],p[4],0.0,1.0,0.0,1.0,NullCompressionModel()),
+    ActiveMaterialAdapter(NullEnergyModel()),
+    RLRSQActiveDeformationGradientModel(0.5),
+    PelceSunLangeveld1995Model())
+)
+fit = curve_fit(model,
+    xdata,
+    ydata,
+    # Take original parameters as initial guess
+    [0.1, 0.1, 0.1, 0.1]
+)
+
+p = fit.param
+ho2009a_refit_passive = GeneralizedHillModel(
+    HolzapfelOgden2009Model(p[1],p[2],p[3],p[4],0.0,1.0,0.0,1.0,NullCompressionModel()),
+    ActiveMaterialAdapter(NullEnergyModel()),
+    RLRSQActiveDeformationGradientModel(0.5),
+    PelceSunLangeveld1995Model()
+)
+plot_equibiaxial(ax2, F -> constitutive_driver(F, f₀, s₀, n₀, 0.0, ho2009a_refit_passive)[1]; λₘᵢₙ=minimum(xdata), λₘₐₓ=maximum(xdata))
+
+# -----
+# add raw data
+scatter!(ax2, linyin1998_4_fiber_passive)
+scatter!(ax2, linyin1998_4_sheet_passive)
 
 # 2. refit total stress with different active energies for the generalized Hill model
 # 3. refilt total stress with the same active energy, but holzapfel-Ogden 2009 passive energy (anisotropic form)
