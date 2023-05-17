41ee25c180a9e9e9b6f98d3b5d3e7d62d0e391d7
termi-official <termi-official@users.noreply.github.com>
2023-05-17 03:02:18 +0200

    Extend IO interface.

--- src/io.jl
+++ src/io.jl
@@ -1,19 +1,33 @@
 """
 """
-struct ParaViewWriter{PVD}
+mutable struct ParaViewWriter{PVD}
     filename::String
     pvd::PVD
+    current_file::Union{WriteVTK.DatasetFile, Nothing}
 end
 
-ParaViewWriter(filename::String) = ParaViewWriter(filename, paraview_collection("$filename.pvd"))
+ParaViewWriter(filename::String) = ParaViewWriter(filename, paraview_collection("$filename.pvd"), nothing)
 
 function store_timestep!(io::ParaViewWriter, t, dh, u)
-    mkpath(io.filename)
-    vtk_grid(io.filename * "/$t.vtu", dh) do vtk
-        vtk_point_data(vtk, dh, u)
-        vtk_save(vtk)
-        io.pvd[t] = vtk
+    if io.current_file === nothing
+        mkpath(io.filename)
+        io.current_file = vtk_grid(io.filename * "/$t.vtu", dh)
     end
+    vtk_point_data(io.current_file, dh, u)
+end
+
+function store_timestep_celldata!(io::ParaViewWriter, t, u, coeff_name::String)
+    if io.current_file === nothing
+        mkpath(io.filename)
+        io.current_file = vtk_grid(io.filename * "/$t.vtu", dh)
+    end
+    vtk_cell_data(io.current_file, u, coeff_name)
+end
+
+function finalize_timestep!(io::ParaViewWriter, t)
+    vtk_save(io.current_file)
+    io.pvd[t] = io.current_file
+    io.current_file = nothing
 end
 
 function finalize!(io::ParaViewWriter)
@@ -27,12 +41,21 @@ struct JLD2Writer{FD}
     fd::FD
 end
 
-JLD2Writer(filename::String, overwrite::Bool=true) = JLD2Writer(filename, jldopen("$filename.jld2", overwrite ? "w" : "a+"; compress = true))
+JLD2Writer(filename::String, overwrite::Bool=true) = JLD2Writer(filename, jldopen("$filename.jld2", overwrite ? "a+" : "w"; compress = true))
 
 function store_timestep!(io::JLD2Writer, t, dh, u)
     t ≈ 0.0 && (io.fd["dh"] = dh)
     io.fd["timesteps/$t/solution"] = u
 end
 
-function finalize!(io::JLD2Writer)
+function store_timestep_field!(io::JLD2Writer, t, dh, u, coeff_name::String)
+    t ≈ 0.0 && (io.fd["dh-$coeff_name"] = dh)
+    io.fd["timesteps/$t/coefficient/$coeff_name"] = u
 end
+
+function store_timestep_celldata!(io::JLD2Writer, t, u, coeff_name::String)
+    io.fd["timesteps/$t/celldata/$coeff_name"] = u
+end
+
+finalize_timestep!(io::JLD2Writer, t) = nothing
+finalize!(io::JLD2Writer) = nothing
