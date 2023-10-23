bb57e656cb2df8dc892094c04fcbf113cc8e2d3a
termi-official <termi-official@users.noreply.github.com>
2023-10-23 20:18:55 +0200

    Format

--- src/modeling/boundary_conditions.jl
+++ src/modeling/boundary_conditions.jl
@@ -18,20 +18,20 @@ struct NormalSpringBC
 end
 
 struct BendingSpringBC
-kᵇ::Float64
-boundary_name::String
+    kᵇ::Float64
+    boundary_name::String
 end
 
 struct ConstantPressureBC
-p::Float64
-boundary_name::String
+    p::Float64
+    boundary_name::String
 end
 
 struct SimpleFaceCache{MP, FV}
-mp::MP
-# microstructure_model::MM
-# coordinate_system::CS
-fv::FV
+    mp::MP
+    # microstructure_model::MM
+    # coordinate_system::CS
+    fv::FV
 end
 
 getboundaryname(face_cache::FC) where {FC} = face_cache.mp.boundary_name
@@ -39,31 +39,31 @@ getboundaryname(face_cache::FC) where {FC} = face_cache.mp.boundary_name
 setup_face_cache(bcd::BCD, fv::FV, time) where {BCD, FV} = SimpleFaceCache(bcd, fv)
 
 function assemble_face!(Kₑ::Matrix, residualₑ, uₑ, face, cache::SimpleFaceCache{RobinBC,FV}, time) where {FV}
-@unpack mp, fv = cache
-@unpack α = mp
+    @unpack mp, fv = cache
+    @unpack α = mp
 
-reinit!(fv, face[1], face[2])
+    reinit!(fv, face[1], face[2])
 
-ndofs_face = getnbasefunctions(fv)
-for qp in 1:getnquadpoints(fv)
-    dΓ = getdetJdV(fv, qp)
-    N = getnormal(fv, qp)
+    ndofs_face = getnbasefunctions(fv)
+    for qp in 1:getnquadpoints(fv)
+        dΓ = getdetJdV(fv, qp)
+        N = getnormal(fv, qp)
 
-    u_q = function_value(fv, qp, uₑ)
-    ∂²Ψ∂u², ∂Ψ∂u = Tensors.hessian(u -> α*u⋅u, u_q, :all)
+        u_q = function_value(fv, qp, uₑ)
+        ∂²Ψ∂u², ∂Ψ∂u = Tensors.hessian(u -> α*u⋅u, u_q, :all)
 
-    # Add contribution to the residual from this test function
-    for i in 1:ndofs_face
-        δuᵢ = shape_value(fv, qp, i)
-        residualₑ[i] += δuᵢ ⋅ ∂Ψ∂u * dΓ
+        # Add contribution to the residual from this test function
+        for i in 1:ndofs_face
+            δuᵢ = shape_value(fv, qp, i)
+            residualₑ[i] += δuᵢ ⋅ ∂Ψ∂u * dΓ
 
-        for j in 1:ndofs_face
-            δuⱼ = shape_value(fv, qp, j)
-            # Add contribution to the tangent
-            Kₑ[i, j] += ( δuᵢ ⋅ ∂²Ψ∂u² ⋅ δuⱼ ) * dΓ
+            for j in 1:ndofs_face
+                δuⱼ = shape_value(fv, qp, j)
+                # Add contribution to the tangent
+                Kₑ[i, j] += ( δuᵢ ⋅ ∂²Ψ∂u² ⋅ δuⱼ ) * dΓ
+            end
         end
     end
-end
 end
 
 function assemble_face!(Kₑ::Matrix, uₑ, face, cache::SimpleFaceCache{RobinBC,FV}, time) where {FV}
@@ -94,31 +94,31 @@ function assemble_face!(Kₑ::Matrix, uₑ, face, cache::SimpleFaceCache{RobinBC
 end
 
 function assemble_face!(Kₑ::Matrix, residualₑ, uₑ, face, cache::SimpleFaceCache{NormalSpringBC,FV}, time) where {FV}
-@unpack mp, fv = cache
-@unpack kₛ = mp
+    @unpack mp, fv = cache
+    @unpack kₛ = mp
 
-reinit!(fv, face[1], face[2])
+    reinit!(fv, face[1], face[2])
 
-ndofs_face = getnbasefunctions(fv)
-for qp in 1:getnquadpoints(fv)
-    dΓ = getdetJdV(fv, qp)
-    N = getnormal(fv, qp)
+    ndofs_face = getnbasefunctions(fv)
+    for qp in 1:getnquadpoints(fv)
+        dΓ = getdetJdV(fv, qp)
+        N = getnormal(fv, qp)
 
-    u_q = function_value(fv, qp, uₑ)
-    ∂²Ψ∂u², ∂Ψ∂u = Tensors.hessian(u -> 0.5*kₛ*(u⋅N)^2, u_q, :all)
+        u_q = function_value(fv, qp, uₑ)
+        ∂²Ψ∂u², ∂Ψ∂u = Tensors.hessian(u -> 0.5*kₛ*(u⋅N)^2, u_q, :all)
 
-    # Add contribution to the residual from this test function
-    for i in 1:ndofs_face
-        δuᵢ = shape_value(fv, qp, i)
-        residualₑ[i] += δuᵢ ⋅ ∂Ψ∂u * dΓ
+        # Add contribution to the residual from this test function
+        for i in 1:ndofs_face
+            δuᵢ = shape_value(fv, qp, i)
+            residualₑ[i] += δuᵢ ⋅ ∂Ψ∂u * dΓ
 
-        for j in 1:ndofs_face
-            δuⱼ = shape_value(fv, qp, j)
-            # Add contribution to the tangent
-            Kₑ[i, j] += ( δuᵢ ⋅ ∂²Ψ∂u² ⋅ δuⱼ ) * dΓ
+            for j in 1:ndofs_face
+                δuⱼ = shape_value(fv, qp, j)
+                # Add contribution to the tangent
+                Kₑ[i, j] += ( δuᵢ ⋅ ∂²Ψ∂u² ⋅ δuⱼ ) * dΓ
+            end
         end
     end
-end
 end
 
 function assemble_face!(Kₑ::Matrix, uₑ, face, cache::SimpleFaceCache{NormalSpringBC,FV}, time) where {FV}
@@ -149,34 +149,34 @@ function assemble_face!(Kₑ::Matrix, uₑ, face, cache::SimpleFaceCache{NormalS
 end
 
 function assemble_face!(Kₑ::Matrix, residualₑ, uₑ, face, cache::SimpleFaceCache{BendingSpringBC,FV}, time) where {FV}
-@unpack mp, fv = cache
-@unpack kᵇ = mp
+    @unpack mp, fv = cache
+    @unpack kᵇ = mp
 
-reinit!(fv, face[1], face[2])
+    reinit!(fv, face[1], face[2])
 
-ndofs_face = getnbasefunctions(fv)
-for qp in 1:getnquadpoints(fv)
-    dΓ = getdetJdV(fv, qp)
-    N = getnormal(fv, qp)
+    ndofs_face = getnbasefunctions(fv)
+    for qp in 1:getnquadpoints(fv)
+        dΓ = getdetJdV(fv, qp)
+        N = getnormal(fv, qp)
 
-    ∇u = function_gradient(fv, qp, uₑ)
-    F = one(∇u) + ∇u
+        ∇u = function_gradient(fv, qp, uₑ)
+        F = one(∇u) + ∇u
 
-    ∂²Ψ∂F², ∂Ψ∂F = Tensors.hessian(F_ -> 0.5*kᵇ*(transpose(inv(F_))⋅N - N)⋅(transpose(inv(F_))⋅N - N), F, :all)
+        ∂²Ψ∂F², ∂Ψ∂F = Tensors.hessian(F_ -> 0.5*kᵇ*(transpose(inv(F_))⋅N - N)⋅(transpose(inv(F_))⋅N - N), F, :all)
 
-    # Add contribution to the residual from this test function
-    for i in 1:ndofs_face
-        ∇δui = shape_gradient(fv, qp, i)
-        residualₑ[i] -= ∇δui ⊡ ∂Ψ∂F * dΓ
+        # Add contribution to the residual from this test function
+        for i in 1:ndofs_face
+            ∇δui = shape_gradient(fv, qp, i)
+            residualₑ[i] -= ∇δui ⊡ ∂Ψ∂F * dΓ
 
-        ∇δui∂P∂F = ∇δui ⊡ ∂²Ψ∂F² # Hoisted computation
-        for j in 1:ndofs_face
-            ∇δuj = shape_gradient(fv, qp, j)
-            # Add contribution to the tangent
-            Kₑ[i, j] += ( ∇δui∂P∂F ⊡ ∇δuj ) * dΓ
+            ∇δui∂P∂F = ∇δui ⊡ ∂²Ψ∂F² # Hoisted computation
+            for j in 1:ndofs_face
+                ∇δuj = shape_gradient(fv, qp, j)
+                # Add contribution to the tangent
+                Kₑ[i, j] += ( ∇δui∂P∂F ⊡ ∇δuj ) * dΓ
+            end
         end
     end
-end
 end
 
 
@@ -211,40 +211,40 @@ function assemble_face!(Kₑ::Matrix, uₑ, face, cache::SimpleFaceCache{Bending
 end
 
 function assemble_face!(Kₑ::Matrix, residualₑ, uₑ, face, cache::SimpleFaceCache{ConstantPressureBC,FV}, time) where {FV}
-@unpack mp, fv = cache
-@unpack p = mp
+    @unpack mp, fv = cache
+    @unpack p = mp
 
-reinit!(fv, face[1], face[2])
+    reinit!(fv, face[1], face[2])
 
-ndofs_face = getnbasefunctions(fv)
-for qp in 1:getnquadpoints(fv)
-    dΓ = getdetJdV(fv, qp)
+    ndofs_face = getnbasefunctions(fv)
+    for qp in 1:getnquadpoints(fv)
+        dΓ = getdetJdV(fv, qp)
 
-    n₀ = getnormal(fv, qp)
+        n₀ = getnormal(fv, qp)
 
-    ∇u = function_gradient(fv, qp, uₑ)
-    F = one(∇u) + ∇u
+        ∇u = function_gradient(fv, qp, uₑ)
+        F = one(∇u) + ∇u
 
-    # ∂P∂F = Tensors.gradient(
-    #     F_ad -> p * det(F_ad) * transpose(inv(F_ad)),
-    # F)
+        # ∂P∂F = Tensors.gradient(
+        #     F_ad -> p * det(F_ad) * transpose(inv(F_ad)),
+        # F)
 
-    # Add contribution to the residual from this test function
-    cofF = transpose(inv(F))
-    neumann_term = p * det(F) * cofF
-    for i in 1:ndofs_face
-        δuᵢ = shape_value(fv, qp, i)
-        residualₑ[i] += neumann_term ⋅ n₀ ⋅ δuᵢ * dΓ
+        # Add contribution to the residual from this test function
+        cofF = transpose(inv(F))
+        neumann_term = p * det(F) * cofF
+        for i in 1:ndofs_face
+            δuᵢ = shape_value(fv, qp, i)
+            residualₑ[i] += neumann_term ⋅ n₀ ⋅ δuᵢ * dΓ
 
-        # ∂P∂Fδui =   ∂P∂F ⊡ (n₀ ⊗ δuᵢ) # Hoisted computation
-        for j in 1:ndofs_face
-            ∇δuⱼ = shape_gradient(fv, qp, j)
-            # Add contribution to the tangent
-            # Kₑ[i, j] += (n₀ ⊗ δuⱼ) ⊡ ∂P∂Fδui * dΓ
-            Kₑ[i, j] += δuᵢ ⋅ (((cofF ⊡ ∇δuⱼ) * one(cofF) - cofF ⋅ transpose(∇δuⱼ)) ⋅ neumann_term) ⋅ n₀ * dΓ
+            # ∂P∂Fδui =   ∂P∂F ⊡ (n₀ ⊗ δuᵢ) # Hoisted computation
+            for j in 1:ndofs_face
+                ∇δuⱼ = shape_gradient(fv, qp, j)
+                # Add contribution to the tangent
+                # Kₑ[i, j] += (n₀ ⊗ δuⱼ) ⊡ ∂P∂Fδui * dΓ
+                Kₑ[i, j] += δuᵢ ⋅ (((cofF ⊡ ∇δuⱼ) * one(cofF) - cofF ⋅ transpose(∇δuⱼ)) ⋅ neumann_term) ⋅ n₀ * dΓ
+            end
         end
     end
-end
 end
 
 
