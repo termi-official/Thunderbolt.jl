33ec374f3681d6e97f6fb93b2f8e2607a966aef5
Abdulaziz Hamid <Abdulaziz.Mohamed@edu.ruhr-uni-bochum.de>
2024-01-14 14:32:54 +0100

    dispatch spatial_coordinate

--- src/quadrature_iterator.jl
+++ src/quadrature_iterator.jl
@@ -35,6 +35,7 @@ Ferrite.shape_value(cv::CellValues, qp::QuadraturePoint, base_fun_idx::Int) = Fe
 Ferrite.shape_gradient(cv::CellValues, qp::QuadraturePoint, base_fun_idx::Int) = Ferrite.shape_gradient(cv, qp.i, base_fun_idx)
 Ferrite.function_value(cv::CellValues, qp::QuadraturePoint, ue) = Ferrite.function_value(cv, qp.i, ue)
 Ferrite.function_gradient(cv::CellValues, qp::QuadraturePoint, ue) = Ferrite.function_gradient(cv, qp.i, ue)
+Ferrite.spatial_coordinate(fe_v::Ferrite.AbstractValues, qp::QuadraturePoint, x::AbstractVector{<:Vec}) = spatial_coordinate(fe_v, qp.i, x)
 
 Ferrite.getdetJdV(fv::FaceValues, qp::QuadraturePoint) = Ferrite.getdetJdV(fv, qp.i)
 Ferrite.shape_value(fv::FaceValues, qp::QuadraturePoint, base_fun_idx::Int) = Ferrite.shape_value(fv, qp.i, base_fun_idx)
