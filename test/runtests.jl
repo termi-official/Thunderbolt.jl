ee55ce05af7f1e48022ed539e15dccf963a69023
AbdulazizAhmed <Abdulaziz.Mohamed@edu.ruhr-uni-bochum.de>
2024-01-17 15:41:00 +0100

    Use `QuadratureIterator` in examples (#56)
    The QuadratureIterator was not used everywhere correctly. This PR fixes this behavior and adds corresponding test coverage.
    
    ---------
    
    Co-authored-by: Dennis Ogiermann <termi-official@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ include("test_operators.jl")
 include("test_type_stability.jl")
 include("test_mesh.jl")
 include("test_coefficients.jl")
+include("test_microstructures.jl")
 
 include("integration/test_contracting_cuboid.jl")
 include("integration/test_waveprop_cuboid.jl")
