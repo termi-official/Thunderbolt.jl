69b0456f9f4efcdb42b141b5efa276c57818b19e
Dennis Ogiermann <termi-official@users.noreply.github.com>
2024-01-17 10:43:20 +0100

    Apply suggestions from code review

--- test/test_microstructures.jl
+++ test/test_microstructures.jl
@@ -4,9 +4,9 @@
 
     ip_fsn = Lagrange{ref_shape, order}()^3
     ip_geo = Lagrange{ref_shape, order}()^3
-    cv = CellValues(QuadratureRule{ref_shape}(1), ip_fsn, ip_geo)
+    cv = CellValues(QuadratureRule{ref_shape}(2), ip_fsn, ip_geo)
 
-    ring_grid = generate_ring_mesh(8,2,2)
+    ring_grid = generate_ring_mesh(20,2,2)
     ring_cs = compute_midmyocardial_section_coordinate_system(ring_grid, ip_geo)
     @testset "OrthotropicMicrostructureModel" begin
         ms = create_simple_microstructure_model(ring_cs, ip_fsn, ip_geo,
@@ -21,6 +21,7 @@
             for qp in QuadratureIterator(cv)
                 coords = getcoordinates(cellcache)
                 sc = spatial_coordinate(cv, qp, coords)
+                # If we set all angles to 0, then the generator on the ring simply generates sheetlets which point in positive z direction, where the normal point radially outwards.
                 ndir = Vec(sc[1:2]..., 0.)/norm(sc[1:2])
                 sdir = Vec(0., 0., 1.)
                 fdir = sdir × ndir
@@ -28,9 +29,9 @@
                 fdir_eval = eval_cooef[1]
                 sdir_eval = eval_cooef[2]
                 ndir_eval = eval_cooef[3]
-                @test fdir_eval ≈ fdir
-                @test sdir_eval ≈ sdir
-                @test ndir_eval ≈ ndir
+                @test fdir_eval ≈ fdir atol=1e-8
+                @test sdir_eval ≈ sdir atol=1e-8
+                @test ndir_eval ≈ ndir atol=1e-8
             end
         end
     end
