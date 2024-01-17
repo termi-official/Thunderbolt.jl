57e15ca8f250615833982ee281438e3e20cb45ea
termi-official <termi-official@users.noreply.github.com>
2024-01-17 15:28:35 +0100

    Change test strategy slightly.

--- test/test_microstructures.jl
+++ test/test_microstructures.jl
@@ -2,12 +2,30 @@
     ref_shape = RefHexahedron
     order = 1
 
+    qr = QuadratureRule{ref_shape}(2)
+
     ip_fsn = Lagrange{ref_shape, order}()^3
     ip_geo = Lagrange{ref_shape, order}()^3
-    cv = CellValues(QuadratureRule{ref_shape}(2), ip_fsn, ip_geo)
+    cv_fsn = CellValues(qr, ip_fsn, ip_geo)
 
-    ring_grid = generate_ring_mesh(20,2,2)
+    ring_grid = generate_ring_mesh(80,1,1)
     ring_cs = compute_midmyocardial_section_coordinate_system(ring_grid, ip_geo)
+
+    cv_cs = Thunderbolt.create_cellvalues(ring_cs, qr)
+
+    @testset "Midmyocardial coordinate system" begin
+        for cellcache in CellIterator(ring_cs.dh)
+            reinit!(cv_cs, cellcache)
+            dof_indices = celldofs(cellcache)
+            for qp in QuadratureIterator(cv_fsn)
+                coords = getcoordinates(cellcache)
+                sc = spatial_coordinate(cv_fsn, qp, coords)
+                transmural = 4*(norm(Vec(sc[1:2]..., 0.))-0.75)
+                @test transmural ≈ function_value(cv_cs, qp, ring_cs.u_transmural[dof_indices]) atol=0.01
+            end
+        end
+    end
+
     @testset "OrthotropicMicrostructureModel" begin
         ms = create_simple_microstructure_model(ring_cs, ip_fsn, ip_geo,
             endo_helix_angle = deg2rad(0.0),
@@ -17,21 +35,18 @@
             sheetlet_pseudo_angle = deg2rad(0)
         )
         for cellcache in CellIterator(ring_grid)
-            reinit!(cv, cellcache)
-            for qp in QuadratureIterator(cv)
+            reinit!(cv_fsn, cellcache)
+            for qp in QuadratureIterator(cv_fsn)
                 coords = getcoordinates(cellcache)
-                sc = spatial_coordinate(cv, qp, coords)
+                sc = spatial_coordinate(cv_fsn, qp, coords)
                 # If we set all angles to 0, then the generator on the ring simply generates sheetlets which point in positive z direction, where the normal point radially outwards.
                 ndir = Vec(sc[1:2]..., 0.)/norm(sc[1:2])
                 sdir = Vec(0., 0., 1.)
                 fdir = sdir × ndir
-                eval_cooef = evaluate_coefficient(ms, cellcache, qp, 0)
-                fdir_eval = eval_cooef[1]
-                sdir_eval = eval_cooef[2]
-                ndir_eval = eval_cooef[3]
-                @test fdir_eval ≈ fdir atol=1e-8
-                @test sdir_eval ≈ sdir atol=1e-8
-                @test ndir_eval ≈ ndir atol=1e-8
+                fsn = evaluate_coefficient(ms, cellcache, qp, 0)
+                @test fsn[1] ≈ fdir atol=0.05
+                @test fsn[2] ≈ sdir
+                @test fsn[3] ≈ ndir atol=0.05
             end
         end
     end
