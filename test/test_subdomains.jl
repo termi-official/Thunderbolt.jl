dcb74146cb215f1714b8753c08f7bde36e11dd3c
Abdulaziz Hamid <Abdulaziz.Mohamed@edu.ruhr-uni-bochum.de>
2024-01-18 16:31:47 +0100

    test

--- test/test_subdomains.jl
+++ test/test_subdomains.jl
@@ -44,14 +44,12 @@
     protocol = AnalyticalTransmembraneStimulationProtocol(analytical_coeff, [SVector((0., 0.))])
     # Dummy writers
     io = ParaViewWriter("")
-    ioJLD2 = JLD2Writer("", 0) # This is wrong, it's not intended to call the constructor but to create a dummy object for testing.
-
+    ioJLD2 = JLD2Writer("") 
+    
     @test_throws subdomain_error LVCoordinateSystem(dh, [0.], [0.])
     @test_throws subdomain_error Thunderbolt.compute_chamber_volume(dh, [0.], "top", Thunderbolt.Hirschvogel2016SurrogateVolume)
     @test_throws subdomain_error Thunderbolt.TransientHeatProblem(Thunderbolt.ConductivityToDiffusivityCoefficient(0., 0., 0.), protocol, dh)
     @test_throws subdomain_error Thunderbolt.QuasiStaticNonlinearProblem(dh, ch, qsm, [])
-    # @test_throws subdomain_error Thunderbolt.QuasiStaticODEProblem(dh, ch, qsm, [])
-    # @test_throws subdomain_error Thunderbolt.QuasiStaticDAEProblem(dh, ch, qsm, [])
     @test_throws subdomain_error Thunderbolt.AssembledNonlinearOperator([0. 0.;0. 0.], element_cache, (), dh)
     @test_throws subdomain_error Thunderbolt.AssembledBilinearOperator([0. 0.;0. 0.], element_cache, dh)
     @test_throws subdomain_error Thunderbolt.LinearOperator([0.], element_cache, dh)
