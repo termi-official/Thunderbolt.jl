6555766b3a1529068aacbfd7ebdfcfadae64c455
Dennis Ogiermann <termi-official@users.noreply.github.com>
2024-01-20 11:39:48 +0100

    RBS

--- src/krck-mesh.jl
+++ src/krck-mesh.jl
@@ -1,22 +1,39 @@
 # Mesh based on "Efficient multi-level hp-finite elements in arbitrary dimensions." (2022)
 using StaticArrays
 
-abstract type AbstractKRCKElement{sdim,N,M} <: AbstractCell{sdim,N,M} end
+abstract type AbstractTreeElement{ref_shape} <: AbstractCell{ref_shape} end
 
-struct KRCKElement{sdim,N,M} <: AbstractKRCKElement{sdim,N,M}
-    active_nodes::MVector{N, Bool}
-    # parent::Int 
-    face_neighbors::MVector{M, Int}
+@enumx RBSTreeLineElementPlacement Iso1 Iso2
+
+struct RBSTreeLineElement <: AbstractTreeElement{RefLine}}
+    parent::Int
     level::Int
-    # children::Union{Nothing, KRCKElement{sdim,N,M}}
+    relative_position::RBSTreeLineElementPlacement
+end
+
+@enumx RBSTreeQuadrilateralElementPlacement Iso11 Iso12 Iso21 Iso22 #AnisoX1 AnisoX2 AnisoY1 AnisoY2
+
+struct RBSTreeQuadrilateralElement <: AbstractCell{RefQuadrilateral}
+    active_vertices::MVector{4, Bool}
+    active_faces::MVector{4, Bool}
+    level::NTuple{2, Int}
+    face_neighbors::MVector{4, Bool}
+end
+
+@enumx RBSTreeQuadrilateralElementPlacement Iso111 Iso112 Iso121 Iso122 Iso211 Iso212 Iso221 Iso222 #AnisoX1 AnisoX2 AnisoY1 AnisoY2
+
+struct RBSTreeHexahedronElement <: AbstractCell{RefHexahedron}
+    active_vertices::MVector{8, Bool}
+    active_faces::MVector{6, Bool}
+    active_edges::MVector{12, Bool}
+    level::NTuple{3, Int}
+    face_neighbors::MVector{6, Bool}
 end
 
 struct KRCKMesh{sdim, E <: KRCKElement, T <: Real} <: Ferrite.AbstractGrid{sdim}
     root_node_coords::Vector{Ferrite.Node{sdim, T}}
     root_elements::Vector{E}
 end
-
-const KRCKQuad2D = KRCKElement{2,4,4}
 
 function refine!(mesh::KRCKMesh, element_idx::Int)
     element_to_refine = mesh.elements[element_idx]
