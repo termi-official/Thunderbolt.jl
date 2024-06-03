8176eb1b6cb021ca7f4822d99c3f82eb66af9d34
termi-official <termi-official@users.noreply.github.com>
2024-06-03 22:58:47 +0200

    Update examples.

--- examples/lv-with-mtk-circuit.jl
+++ examples/lv-with-mtk-circuit.jl
@@ -3,165 +3,9 @@ using CirculatorySystemModels.OrdinaryDiffEq, ModelingToolkit, CirculatorySystem
 using GLMakie
 
 using Thunderbolt
+import Thunderbolt: OS
 
-
-# TODO refactor this one into the framework code and put a nice abstraction layer around it
-struct StandardMechanicalIOPostProcessorLFSI{IO, CVC, CSC, AX}
-    io::IO
-    cvc::CVC
-    csc::CSC
-    ax::AX
-end
-
-function (postproc::StandardMechanicalIOPostProcessorLFSI)(t, problem::Thunderbolt.SplitProblem, solver_cache)
-    (postproc::StandardMechanicalIOPostProcessorLFSI)(t, problem.A, solver_cache.A_solver_cache)
-    (postproc::StandardMechanicalIOPostProcessorLFSI)(t, problem.B, solver_cache.B_solver_cache)
-
-    @unpack tying_problem = problem.A
-    lv = tying_problem.chambers[1]
-    scatter!(postproc.ax[1], lv.V⁰ᴰval, solver_cache.A_solver_cache.uₙ[lv.pressure_dof_index])
-    # lines!(postproc.ax[2], solver_cache.uₙ[RV.V], solver_cache.uₙ[RV.p])
-    # lines!(postproc.ax[3], solver_cache.uₙ[LA.V], solver_cache.uₙ[LA.p])
-    # lines!(postproc.ax[4], solver_cache.uₙ[RA.V], solver_cache.uₙ[RA.p])
-end
-
-
-function (postproc::StandardMechanicalIOPostProcessorLFSI)(t, problem::Thunderbolt.ODEProblem, solver_cache)
-    @info "Lumped Circuit Solution Vector: $(solver_cache.uₙ)"
-end
-
-function (postproc::StandardMechanicalIOPostProcessorLFSI)(t, problem::Thunderbolt.RSAFDQ20223DProblem, solver_cache)
-    @unpack dh, constitutive_model = problem.structural_problem
-    grid = Ferrite.get_grid(dh)
-    @unpack io, cvc, csc = postproc
-    
-    # Compute some elementwise measures
-    E_ff = zeros(getncells(grid))
-    E_ff2 = zeros(getncells(grid))
-    E_cc = zeros(getncells(grid))
-    E_ll = zeros(getncells(grid))
-    E_rr = zeros(getncells(grid))
-
-    Jdata = zeros(getncells(grid))
-
-    frefdata = zero(Vector{Ferrite.Vec{3}}(undef, getncells(grid)))
-    srefdata = zero(Vector{Ferrite.Vec{3}}(undef, getncells(grid)))
-    fdata = zero(Vector{Ferrite.Vec{3}}(undef, getncells(grid)))
-    sdata = zero(Vector{Ferrite.Vec{3}}(undef, getncells(grid)))
-    helixangledata = zero(Vector{Float64}(undef, getncells(grid)))
-    helixanglerefdata = zero(Vector{Float64}(undef, getncells(grid)))
-
-    # Compute some elementwise measures
-    for sdh ∈ dh.subdofhandlers
-        field_idx = Ferrite.find_field(sdh, :displacement)
-        field_idx === nothing && continue 
-        cv = getcellvalues(cvc, dh.grid.cells[first(sdh.cellset)])
-        for cell ∈ CellIterator(sdh)
-
-            Ferrite.reinit!(cv, cell)
-            global_dofs = celldofs(cell)
-            field_dofs  = dof_range(sdh, field_idx)
-            uₑ = solver_cache.uₙ[global_dofs] # element dofs
-
-            E_ff_cell = 0.0
-            E_cc_cell = 0.0
-            E_rr_cell = 0.0
-            E_ll_cell = 0.0
-
-            Jdata_cell = 0.0
-
-            frefdata_cell = Ferrite.Vec{3}((0.0, 0.0, 0.0))
-            srefdata_cell = Ferrite.Vec{3}((0.0, 0.0, 0.0))
-            fdata_cell = Ferrite.Vec{3}((0.0, 0.0, 0.0))
-            sdata_cell = Ferrite.Vec{3}((0.0, 0.0, 0.0))
-            helixangle_cell = 0.0
-            helixangleref_cell = 0.0
-
-            nqp = getnquadpoints(cv)
-            for qp in QuadratureIterator(cv)
-                dΩ = getdetJdV(cv, qp)
-
-                # Compute deformation gradient F
-                ∇u = function_gradient(cv, qp, uₑ)
-                F = one(∇u) + ∇u
-
-                C = tdot(F)
-                E = (C-one(C))/2.0
-                f₀,s₀,n₀ = evaluate_coefficient(constitutive_model.microstructure_model, cell, qp, time)
-
-                E_ff_cell += f₀ ⋅ E ⋅ f₀
-
-                f₀_current = F⋅f₀
-                f₀_current /= norm(f₀_current)
-
-                s₀_current = F⋅s₀
-                s₀_current /= norm(s₀_current)
-
-                coords = getcoordinates(cell)
-                x_global = spatial_coordinate(cv, qp, coords)
-
-                # v_longitudinal = function_gradient(cv_cs, qp, coordinate_system.u_apicobasal[celldofs(cell)])
-                # v_radial = function_gradient(cv_cs, qp, coordinate_system.u_transmural[celldofs(cell)])
-                # v_circimferential = v_longitudinal × v_radial
-                # @TODO compute properly via coordinate system
-                v_longitudinal = Ferrite.Vec{3}((0.0, 0.0, 1.0))
-                v_radial = Ferrite.Vec{3}((x_global[1],x_global[2],0.0))
-                v_radial /= norm(v_radial)
-                v_circimferential = v_longitudinal × v_radial # Ferrite.Vec{3}((x_global[2],x_global[1],0.0))
-                v_circimferential /= norm(v_circimferential)
-                #
-                E_ll_cell += v_longitudinal ⋅ E ⋅ v_longitudinal
-                E_rr_cell += v_radial ⋅ E ⋅ v_radial
-                E_cc_cell += v_circimferential ⋅ E ⋅ v_circimferential
-
-                Jdata_cell += det(F)
-
-                frefdata_cell += f₀
-                srefdata_cell += s₀
-
-                fdata_cell += f₀_current
-                sdata_cell += s₀_current
-
-                helixangle_cell += acos(clamp(f₀_current ⋅ v_circimferential, -1.0, 1.0)) * sign((v_circimferential × f₀_current) ⋅ v_radial)
-                helixangleref_cell += acos(clamp(f₀ ⋅ v_circimferential, -1.0, 1.0)) * sign((v_circimferential × f₀) ⋅ v_radial)
-            end
-
-            E_ff[Ferrite.cellid(cell)] = E_ff_cell / nqp
-            E_cc[Ferrite.cellid(cell)] = E_cc_cell / nqp
-            E_rr[Ferrite.cellid(cell)] = E_rr_cell / nqp
-            E_ll[Ferrite.cellid(cell)] = E_ll_cell / nqp
-            Jdata[Ferrite.cellid(cell)] = Jdata_cell / nqp
-            frefdata[Ferrite.cellid(cell)] = frefdata_cell / nqp
-            frefdata[Ferrite.cellid(cell)] /= norm(frefdata[Ferrite.cellid(cell)])
-            srefdata[Ferrite.cellid(cell)] = srefdata_cell / nqp
-            srefdata[Ferrite.cellid(cell)] /= norm(srefdata[Ferrite.cellid(cell)])
-            fdata[Ferrite.cellid(cell)] = fdata_cell / nqp
-            fdata[Ferrite.cellid(cell)] /= norm(fdata[Ferrite.cellid(cell)])
-            sdata[Ferrite.cellid(cell)] = sdata_cell / nqp
-            sdata[Ferrite.cellid(cell)] /= norm(sdata[Ferrite.cellid(cell)])
-            helixanglerefdata[Ferrite.cellid(cell)] = helixangleref_cell / nqp
-            helixangledata[Ferrite.cellid(cell)] = helixangle_cell / nqp
-        end
-    end
-
-    # Save the solution
-    Thunderbolt.store_timestep!(io, t, dh.grid)
-    # Thunderbolt.store_timestep_field!(io, t, dh, solver_cache.uₙ, :displacement)
-    Thunderbolt.store_timestep_field!(io, t, dh, solver_cache.uₙ.blocks[1], :displacement)
-    Thunderbolt.store_timestep_celldata!(io, t, hcat(frefdata...),"Reference Fiber Data")
-    Thunderbolt.store_timestep_celldata!(io, t, hcat(fdata...),"Current Fiber Data")
-    Thunderbolt.store_timestep_celldata!(io, t, hcat(srefdata...),"Reference Sheet Data")
-    Thunderbolt.store_timestep_celldata!(io, t, hcat(sdata...),"Current Sheet Data")
-    Thunderbolt.store_timestep_celldata!(io, t, E_ff,"E_ff")
-    Thunderbolt.store_timestep_celldata!(io, t, E_ff2,"E_ff2")
-    Thunderbolt.store_timestep_celldata!(io, t, E_cc,"E_cc")
-    Thunderbolt.store_timestep_celldata!(io, t, E_rr,"E_rr")
-    Thunderbolt.store_timestep_celldata!(io, t, E_ll,"E_ll")
-    Thunderbolt.store_timestep_celldata!(io, t, Jdata,"J")
-    Thunderbolt.store_timestep_celldata!(io, t, rad2deg.(helixangledata),"Helix Angle")
-    Thunderbolt.store_timestep_celldata!(io, t, rad2deg.(helixanglerefdata),"Helix Angle (End Diastole)")
-    Thunderbolt.finalize_timestep!(io, t)
-end
+using UnPack
 
 """
     PressureCouplingChamber(;name)
@@ -387,64 +231,12 @@ circ_sys = structural_simplify(circ_model)
 # FIXME this is illegal. Figure out how to do the transfer correctly.
 u0new = copy(circ_sol_init.u[end])
 
-function solve_ideal_lv_with_circuit(name_base, constitutive_model, grid, coordinate_system, face_models, ip_mech::Thunderbolt.VectorInterpolationCollection, qr_collection::QuadratureRuleCollection, u0new, p3D, V0D, Δt, T)
-    io = ParaViewWriter(name_base);
-
-    solid = StructuralModel(:displacement, constitutive_model, face_models)
-    fluid = MTKLumpedCicuitModel(circ_sys, u0new, [p3D])
-    coupler = LumpedFluidSolidCoupler(
-        [
-            ChamberVolumeCoupling(
-                "Endocardium",
-                RSAFDQ2022SurrogateVolume(),
-                V0D
-            )
-        ],
-        :displacement
-    )
-
-    problem = semidiscretize(
-        RSAFDQ2022Split(RSAFDQ2022Model(solid,fluid,coupler)),
-        FiniteElementDiscretization(
-            Dict(:displacement => ip_mech),
-            # Dirichlet[],
-            [
-                Dirichlet(:displacement, getfaceset(grid, "Base"), (x,t) -> [0.0], [3]),
-                Dirichlet(:displacement, getnodeset(grid, "MyocardialAnchor1"), (x,t) -> (0.0, 0.0, 0.0), [1,2,3]),
-                Dirichlet(:displacement, getnodeset(grid, "MyocardialAnchor2"), (x,t) -> (0.0, 0.0), [2,3]),
-                Dirichlet(:displacement, getnodeset(grid, "MyocardialAnchor3"), (x,t) -> (0.0,), [3]),
-                Dirichlet(:displacement, getnodeset(grid, "MyocardialAnchor4"), (x,t) -> (0.0,), [3])
-            ],
-        ),
-        grid
-    )
-
-    # Postprocessor
-    cv_post = CellValueCollection(qr_collection, ip_mech)
-    standard_postproc = StandardMechanicalIOPostProcessorLFSI(io, cv_post, CoordinateSystemCoefficient(coordinate_system), axs)
-
-    # Create sparse matrix and residual vector
-    solver = LTGOSSolver(
-        LoadDrivenSolver(NewtonRaphsonSolver(;max_iter=100, tol=1e-2)),
-        ForwardEulerSolver(ceil(Int, Δt/0.001)), # Force time step to about 0.001
-    )
-
-    Thunderbolt.solve(
-        problem,
-        solver,
-        Δt, 
-        (0.0, T),
-        default_initializer,
-        standard_postproc
-    )
-end
-
 scaling_factor = 2.7
 LV_grid = Thunderbolt.hexahedralize(Thunderbolt.generate_ideal_lv_mesh(15,2,7; inner_radius = Float64(scaling_factor*0.7), outer_radius = Float64(scaling_factor*1.0), longitudinal_upper = Float64(scaling_factor*0.2), apex_inner = Float64(scaling_factor*1.3), apex_outer = Float64(scaling_factor*1.5)))
 
 order = 1
 intorder = max(2*order-1,2)
-ip_u = LagrangeCollection{order}()^3
+ip_mech = LagrangeCollection{order}()^3
 qr_u = QuadratureRuleCollection(intorder-1)
 LV_cs = compute_LV_coordinate_system(LV_grid)
 LV_fm = create_simple_microstructure_model(LV_cs, LagrangeCollection{1}()^3, endo_helix_angle = deg2rad(-60.0), epi_helix_angle = deg2rad(70.0), endo_transversal_angle = deg2rad(10.0), epi_transversal_angle = deg2rad(-20.0))
@@ -469,21 +261,132 @@ function calcium_profile_function(x,t)
     end
 end
 
+constitutive_model = ActiveStressModel(
+    passive_model,
+    PiersantiActiveStress(;Tmax=2.0e3),
+    PelceSunLangeveld1995Model(;calcium_field=AnalyticalCoefficient(
+        calcium_profile_function,
+        CoordinateSystemCoefficient(LV_cs)
+    )),
+    LV_fm,
+)
+
+name_base = "lv_with_lumped_circuit"
+
+p3D = LVc.p3D
+V0D = LVc.V
+dt₀ = 1.0
+dtvis = 5.0
+tspan = (0.0, 1000.0)
+
+io = ParaViewWriter(name_base);
+
+face_models = ()
+solid = StructuralModel(:displacement, constitutive_model, face_models)
+fluid = MTKLumpedCicuitModel(circ_sys, u0new, [p3D])
+coupler = LumpedFluidSolidCoupler(
+    [
+        ChamberVolumeCoupling(
+            "Endocardium",
+            RSAFDQ2022SurrogateVolume(),
+            V0D
+        )
+    ],
+    :displacement
+)
+
+coupledform = semidiscretize(
+    RSAFDQ2022Split(RSAFDQ2022Model(solid,fluid,coupler)),
+    FiniteElementDiscretization(
+        Dict(:displacement => ip_mech),
+        [
+            Dirichlet(:displacement, getfaceset(LV_grid, "Base"), (x,t) -> [0.0], [3]),
+            Dirichlet(:displacement, getnodeset(LV_grid, "MyocardialAnchor1"), (x,t) -> (0.0, 0.0, 0.0), [1,2,3]),
+            Dirichlet(:displacement, getnodeset(LV_grid, "MyocardialAnchor2"), (x,t) -> (0.0, 0.0), [2,3]),
+            Dirichlet(:displacement, getnodeset(LV_grid, "MyocardialAnchor3"), (x,t) -> (0.0,), [3]),
+            Dirichlet(:displacement, getnodeset(LV_grid, "MyocardialAnchor4"), (x,t) -> (0.0,), [3])
+        ],
+    ),
+    LV_grid
+)
+
+offset = Thunderbolt.solution_size(coupledform.A)
+splitfun = OS.GenericSplitFunction(
+    (
+        coupledform.A,
+        coupledform.B
+    ),
+    (
+        1:offset,
+        (offset+1):(offset+Thunderbolt.solution_size(coupledform.B))
+    ),
+    (
+        VolumeTransfer0D3D(coupledform.A.tying_problem),
+        PressureTransfer3D0D(coupledform.A.tying_problem),
+    ),
+)
+
+# Create sparse matrix and residual vector
+timestepper = OS.LieTrotterGodunov((
+        LoadDrivenSolver(NewtonRaphsonSolver(;max_iter=100, tol=1e-2)),
+        ForwardEulerSolver(ceil(Int, dt₀/0.001)), # Force time step to about 0.001
+))
+
+u₀ = [zeros(offset); u0new]
+
+problem = OS.OperatorSplittingProblem(splitfun, u₀, tspan)
+
+integrator = OS.init(problem, timestepper, dt=dt₀, verbose=true)
+
+f2 = Figure()
+axs = [
+    Axis(f2[1, 1], title="LV"),
+    Axis(f2[1, 2], title="RV"),
+    Axis(f2[2, 1], title="LA"),
+    Axis(f2[2, 2], title="RA")
+]
+
+vlv = Observable(Float64[])
+plv = Observable(Float64[])
+
+vrv = Observable(Float64[])
+prv = Observable(Float64[])
+
+vla = Observable(Float64[])
+pla = Observable(Float64[])
+
+vra = Observable(Float64[])
+pra = Observable(Float64[])
+
+lines!(axs[1], vlv, plv)
+lines!(axs[2], vrv, prv)
+lines!(axs[3], vla, pla)
+lines!(axs[4], vra, pra)
+for i in 1:4
+    xlims!(axs[1], 0.0, 180.0)
+    ylims!(axs[1], 0.0, 180.0)
+end
+display(f2)
+
+io = ParaViewWriter("lv_with_lumped_circuit");
 using Thunderbolt.TimerOutputs
 TimerOutputs.enable_debug_timings(Thunderbolt)
 TimerOutputs.reset_timer!()
-solve_ideal_lv_with_circuit("lv_with_lumped_circuit",
-    ActiveStressModel(
-        passive_model,
-        PiersantiActiveStress(;Tmax=2.0e3),
-        PelceSunLangeveld1995Model(;calcium_field=AnalyticalCoefficient(
-            calcium_profile_function,
-            CoordinateSystemCoefficient(LV_cs)
-        )),
-        LV_fm,
-    ), LV_grid, LV_cs,
-    integral_bcs,
-    ip_u, qr_u, u0new, LVc.p3D, LVc.V, 1.0, 1000.0
-)
+for (u, t) in OS.TimeChoiceIterator(integrator, tspan[1]:dtvis:tspan[2])
+    dh = coupledform.A.structural_problem.dh
+    store_timestep!(io, t, dh.grid)
+    Thunderbolt.store_timestep_field!(io, t, dh, u[1:ndofs(dh)], :displacement) # TODO allow views
+    Thunderbolt.finalize_timestep!(io, t)
+
+    if t > 0.0
+        lv = coupledform.A.tying_problem.chambers[1]
+        append!(vlv.val, lv.V⁰ᴰval)
+        append!(plv.val, u[lv.pressure_dof_index_global])
+        notify(vlv)
+        notify(plv)
+    end
+
+    # TODO other chambers
+end
 TimerOutputs.print_timer()
 TimerOutputs.disable_debug_timings(Thunderbolt)
