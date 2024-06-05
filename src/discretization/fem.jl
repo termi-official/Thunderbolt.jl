51210a5a9e4dba6408e9157ecb01f83dec616b9d
termi-official <termi-official@users.noreply.github.com>
2024-06-05 12:27:10 +0200

    Forgot to move function out

--- src/discretization/fem.jl
+++ src/discretization/fem.jl
@@ -73,29 +73,3 @@ function semidiscretize(model::StructuralModel{<:QuasiStaticModel}, discretizati
 
     return semidiscrete_problem
 end
-
-function create_chamber_tyings(coupler::LumpedFluidSolidCoupler{CVM}, structural_problem, circuit_model) where CVM
-    num_unknowns_structure = solution_size(structural_problem)
-    chamber_tyings = RSAFDQ2022SingleChamberTying{CVM}[]
-    for i in 1:length(coupler.chamber_couplings)
-        # Get i-th ChamberVolumeCoupling
-        coupling = coupler.chamber_couplings[i]
-        # The pressure dof is just the last dof index for the structurel problem + the current chamber index
-        pressure_dof_index = num_unknowns_structure + i
-        chamber_faceset = getfaceset(structural_problem.dh.grid, coupling.chamber_surface_setname)
-        chamber_volume_idx_lumped = get_variable_symbol_index(circuit_model, coupling.lumped_model_symbol)
-        initial_volume_lumped = NaN # We do this to catch initializiation issues downstream
-        tying = RSAFDQ2022SingleChamberTying(
-            pressure_dof_index,
-            pressure_dof_index,
-            chamber_faceset,
-            coupling.chamber_volume_method,
-            coupler.displacement_symbol,
-            initial_volume_lumped,
-            chamber_volume_idx_lumped,
-            num_unknowns_structure+num_unknown_pressures(circuit_model)+chamber_volume_idx_lumped,
-        )
-        push!(chamber_tyings, tying)
-    end
-    return chamber_tyings
-end
