934a4d30a2340c42a1535b64158fbed5b92226f5
Abdulaziz <Abdulaziz.Mohamed@edu.ruhr-uni-bochum.de>
2024-08-30 12:15:11 +0200

    remove unused type

--- src/solver/time/rtc.jl
+++ src/solver/time/rtc.jl
@@ -1,5 +1,3 @@
-abstract type AbstractTimeAdaptionAlgorithm end
-
 """
     ReactionTangentController{LTG <: OS.LieTrotterGodunov, T <: Real} <: OS.AbstractOperatorSplittingAlgorithm
 A timestep length controller for [`LieTrotterGodunov`](@ref) [Lie:1880:tti,Tro:1959:psg,God:1959:dmn](@cite)
