f451c6d09ac93e20b506b70772e017c00c0e467a
Documenter.jl <documenter@juliadocs.github.io>
2025-02-18 21:29:08 +0000

    build based on e777f0c

---
+++ previews/PR188/literate-howto/custom-energies.jl
@@ -0,0 +1,4 @@
+# # [Adding Energies](@id how-to-custom-energy)
+
+# !!! todo
+#     Finalize the element interface and document some examples here.
---
+++ previews/PR188/literate-tutorials/ep03_bidomain.jl
@@ -0,0 +1,30 @@
+# # [Electrophysiology Tutorial 3: Defibrillation with Bidomain Models](@id ep-tutorial_defibrillation)
+#
+# !!! todo
+#     Show ECG and shock.
+#
+# This tutorial shows how to setup Bidomain models and how to induce Spitzenumkehrtachykardie.
+#
+# !!! todo
+#     Provide context.
+#
+# ## Commented Program
+
+# !!! todo
+#     Implement bidomain model. Should be quite easy.
+#     We should also showcase subdomain managmeent and some ECG here.
+
+#md # ## References
+#md # ```@bibliography
+#md # Pages = ["ep03_bidomain.md"]
+#md # Canonical = false
+#md # ```
+
+#md # ## [Plain program](@id ep-tutorial_bidomain-plain-program)
+#md #
+#md # Here follows a version of the program without any comments.
+#md # The file is also available here: [`ep03_bidomain.jl`](ep03_bidomain.jl).
+#md #
+#md # ```julia
+#md # @__CODE__
+#md # ```
