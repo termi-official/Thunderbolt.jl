dde869b1b41aff62bd713c3e809070e7d4da3609
Documenter.jl <documenter@juliadocs.github.io>
2026-09-07 15:08:44 +0000

    build based on db2e400

---
+++ previews/PR272/literate-tutorials/ep05_eikonal.jl
@@ -0,0 +1,27 @@
+# # [Electrophysiology Tutorial 5: Eikonal Models](@id ep-tutorial_eikonal)
+#
+# !!! todo
+#     Show activation timings.
+#
+# This tutorial shows how to solve Eikonal models and how to recover transemembrane potential fields.
+#
+# !!! todo
+#     Provide context.
+#
+# ## Commented Program
+
+
+#md # ## References
+#md # ```@bibliography
+#md # Pages = ["ep05_eikonal.md"]
+#md # Canonical = false
+#md # ```
+
+#md # ## [Plain program](@id ep-tutorial_eikonal-plain-program)
+#md #
+#md # Here follows a version of the program without any comments.
+#md # The file is also available here: [`ep05_eikonal.jl`](ep05_eikonal.jl).
+#md #
+#md # ```julia
+#md # @__CODE__
+#md # ```
